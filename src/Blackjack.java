d3c4d4605406f5d029af41a9e3e19fb53d96cb01
Piran00 <jakob.daanay@gmail.com>
2024-11-15 14:02:43 +0100

    Added moa stuff

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Blackjack {
     List<Card> dealerHand;
     List<Card> playerHand;
@@ -7,12 +11,16 @@ public class Blackjack {
     int dealerHandValue;
     int betAmount;
     Deck deck;
+    Scanner scanner = new Scanner(System.in);
+    String output;
 
 
     public Blackjack(int betAmount) {
         this.betAmount = betAmount;
         dealerHand = new ArrayList<>();
         playerHand = new ArrayList<>();
+
+
         deck = new Deck();
     }
 
@@ -21,6 +29,7 @@ public class Blackjack {
         dealerHandValue = countCards(dealerHand);
         playerHand.addAll(deck.drawCards(2));
         playerHandValue = countCards(playerHand);
+        gameTurn();
     }
     public int countCards(List<Card> hand) {
         int x = 0;
@@ -28,6 +37,72 @@ public class Blackjack {
             x += card.val;
         }
         return x;
+    }
+
+    public void gameTurn() {
+        clearScreen();
+        System.out.println("Your hand");
+        printPlayerHand();
+        System.out.println("Dealer hand");
+        dealerHand.get(0).DisplayCard();
+
+        System.out.println("Stand(s) or hit(h)");
+
+
+        output = scanner.nextLine();
+
+
+        if (Objects.equals(output, "h")) {
+            playerHand.add( deck.drawCard());
+
+            updateVals();
+
+            printPlayerHand();
+            if( playerHandValue > 21) {
+                clearScreen();
+                printPlayerHand();
+                System.out.println("You loose");
+                System.exit(0);
+            }
+            else {
+                gameTurn();
+            }
+        } else if (Objects.equals(output, "s")) {
+            endGameTurn();
+        }
+
+    }
+
+    public void updateVals() {
+        dealerHandValue = countCards(dealerHand);
+        playerHandValue = countCards(playerHand);
+
+    }
+    public void printPlayerHand() {
+        for (Card card : playerHand) {
+            card.DisplayCard();
+        }
+    }
+
+
+    public void endGameTurn() {
+
+    }
+
+
+
+    public static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                // Use ANSI escape codes for Unix-like systems
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@
 import java.util.List;
 public class Main {
     public static void main(String[] args) {
-    Deck deck = new Deck();
-    deck.Shuffle(52);
-    List<Card> hand = deck.drawCards(2);
-    System.out.println( hand.get(1).suit);
-    deck.drawCard().DisplayCard();
-    Blackjack game =new Blackjack(5);
+        Blackjack game = new Blackjack(4);
         game.gameStart();
-        System.out.println(game.playerHandValue);
         }
     }
